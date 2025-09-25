f64fee32f2da9076ea26969cd30fc5c2f65f4a12
Pankaj Bhardwaj <pankajbhj@gmail.com>
2025-09-25 22:45:03 +0530

    Create tsconfig1.ts

---
+++ src/tsconfig1.ts
@@ -0,0 +1,16 @@
+{
+  "compilerOptions": {
+    "jsx": "react-jsx",
+    "esModuleInterop": true,
+    "skipLibCheck": true,
+    "forceConsistentCasingInFileNames": true,
+    "strict": true,
+    "moduleResolution": "node",
+    "target": "ES2021",
+    "module": "ESNext"
+  },
+  "include": [
+    "index.ts",
+    "react-form/**/*"
+  ]
+}
